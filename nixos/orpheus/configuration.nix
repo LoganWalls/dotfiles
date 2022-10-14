e9637bf81c61eca17ce0dcd39b5d30a232c4c0b8
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2022-10-14 09:38:27 -0400

    remove redundant option (already enabled by wlr.enable)

--- nixos/orpheus/configuration.nix
+++ nixos/orpheus/configuration.nix
@@ -92,9 +92,6 @@
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
-    ];
   };
   # Start sway automatically
   environment.loginShellInit = ''
