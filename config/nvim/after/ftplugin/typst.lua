9feb3336b0c37c1922b9cc0adb5917ba70b32cbf
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2024-05-26 13:52:40 -0400

    nvim: update typst config

--- config/nvim/after/ftplugin/typst.lua
+++ config/nvim/after/ftplugin/typst.lua
@@ -1,15 +1,17 @@
 local set = vim.opt_local
 vim.bo.commentstring = "// %s"
-
 set.spell = true -- Enable spellchecking
+
+require("snippets.common").enable_ft("typst")
+
 vim.keymap.set("n", "<Leader>lp", function()
 	-- Pin `main.typ` as the main file if it exists
 	local main_file = vim.fs.find("main.typ", { path = vim.fn.getcwd(), type = "file" })[1]
 	if main_file ~= nil then
-		vim.lsp.buf.execute_command({
+		vim.lsp.buf_request_sync(0, "workspace/executeCommand", {
 			command = "typst-lsp.doPinMain",
 			arguments = { vim.uri_from_fname(main_file) },
-		})
+		}, 1000)
 		print("Pinned to " .. vim.uri_from_fname(main_file))
 	end
 end)
