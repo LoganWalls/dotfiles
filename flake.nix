3e1610f3d0501b5d79f5aa1b4de21975ed3b14f7
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2024-11-17 22:05:19 -0500

    remove home-manager

--- flake.nix
+++ flake.nix
@@ -6,15 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hardware.url = "github:nixos/nixos-hardware";
 
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    # Darwin
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-    nur.url = "github:nix-community/NUR";
+    flakey-profile.url = "github:lf-/flakey-profile";
 
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
     neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
@@ -32,10 +24,10 @@
   outputs = {
     self,
     nixpkgs,
-    home-manager,
+    flakey-profile,
     ...
   } @ inputs: let
-    lib = nixpkgs.lib // home-manager.lib;
+    inherit (nixpkgs) lib;
     forAllSystems = lib.genAttrs [
       "aarch64-linux"
       "i686-linux"
@@ -46,14 +38,12 @@
   in rec {
     overlays = {
       default = import ./overlay {inherit inputs;};
-      nur = inputs.nur.overlay;
       emacs = inputs.emacs-overlay.overlay;
       neovim = inputs.neovim-nightly-overlay.overlays.default;
     };
 
     nixosModules = import ./modules/nixos;
     homeManagerModules = import ./modules/home-manager;
-    darwinModules = import ./modules/darwin;
 
     devShells = forAllSystems (system: {
       default = legacyPackages.${system}.callPackage ./shell.nix {};
@@ -72,6 +62,142 @@
         }
     );
 
+    packages = forAllSystems (system: {
+      profile = flakey-profile.lib.mkProfile rec {
+        pkgs = legacyPackages."${system}";
+        # Specifies things to pin in the flake registry and in NIX_PATH.
+        pinned = {nixpkgs = toString nixpkgs;};
+
+        paths = let
+          homeDirectory =
+            if pkgs.stdenv.isLinux
+            then "/home/logan"
+            else "/Users/logan";
+        in
+          with pkgs; [
+            git
+
+            ### GUI Apps
+            wezterm
+            (nushell.overrideAttrs (old: {
+              # Override forces nushell to use XDG paths (defaults to Application Support on macOS)
+              buildInputs = old.buildInputs ++ [makeWrapper];
+              postInstall =
+                old.postInstall
+                or ""
+                + ''
+                  wrapProgram "$out/bin/nu" --set XDG_CONFIG_HOME "${homeDirectory}/.config"
+                '';
+            }))
+            nushellPlugins.skim
+            (neovim.overrideAttrs (old: {
+              propagatedBuildInputs =
+                (old.propagatedBuildInputs or [])
+                ++ [
+                  pkgs.stdenv.cc.cc # C compiler for tree-sitter grammars
+                ];
+            }))
+            starship
+            carapace
+            direnv
+            nix-direnv
+            zoxide
+
+            ### Shell tools
+            age # encryption
+            btop # system activity monitor
+            bat # cat with highlighting
+            bat-extras.batman # use bat to highlight manpages
+            coreutils-prefixed # for compat with emacs
+            eza # modern ls
+            delta # modern diff
+            fd # modern find
+            flavours # base24 colorscheme management
+            sd # modern sed
+            ripgrep # modern grep
+            just # command runner
+            ugrep # faster grep with backward-compatible interface
+            tealdeer # tldr for manpages
+            macchina # more performant neofetch alternative
+            # mosh # nice ssh sessions
+            gitui # a nice git TUI
+            vivid # ls_colors
+            xclip # work with the system clipboards
+
+            ### File format-specific tools
+            jq # work with json files
+            xsv # work with (c/t)sv files
+            imagemagick # work with images
+            ffmpeg # work images, audio, and video
+            librsvg # allows rasterizing SVGs
+
+            ##### Language-specific
+            ### Shell
+            shellcheck # linter
+            shfmt # formatter
+
+            ### Nix
+            nil # language server
+            nix-direnv # nix intergration for direnv
+            alejandra # black-inspired formatting
+            statix # linter
+
+            ### C
+            cmake
+
+            ### Python
+            uv # python package / env manager
+            (python3.withPackages (ps: with ps; [ipython])) # base interpreter
+            pyright # language server for static type analysis
+            ruff-lsp # language server for everything else
+
+            ### TOML
+            taplo
+
+            ### Lua
+            lua-language-server
+            stylua # formatter
+
+            ### Docker
+            nodePackages.dockerfile-language-server-nodejs
+
+            ### Web
+            nodePackages.vscode-langservers-extracted
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+            nodePackages.prettier # formatter
+
+            ### Tex
+            # texlive.combined.scheme-full # tex distribution
+            # texlab # language server for tex
+
+            ### Typst
+            typst
+            tinymist # language server
+            typstfmt
+            zathura # pdf viewer
+
+            ### Other
+            buf-language-server # protobuf
+
+            ### Prose / writing
+            vale # "linter" for prose
+
+            ### Fonts
+            (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono" "Overpass" "NerdFontsSymbolsOnly"];})
+            apple-fonts
+            comic-mono
+            crimson-pro
+            fira-go
+            fira-sans
+            ibm-plex
+            iosevka-comfy.comfy
+            noto-fonts-emoji
+            sf-mono-liga
+          ];
+      };
+    });
+
     nixosConfigurations = {
       orpheus = nixpkgs.lib.nixosSystem {
         pkgs = legacyPackages.x86_64-linux;
@@ -89,36 +215,6 @@
           (builtins.attrValues nixosModules)
           ++ [
             ./host/sebastian/configuration.nix
-          ];
-      };
-    };
-
-    homeConfigurations = let
-      useFlakeNixpkgs = {
-        nix.registry.nixpkgs.flake = nixpkgs; # Make CLI use the same nixpkgs commit as this config
-      };
-    in {
-      "logan@makoto" = let
-        pkgs = legacyPackages.aarch64-darwin;
-      in
-        home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = {inherit inputs;};
-          modules =
-            (builtins.attrValues homeManagerModules)
-            ++ [
-              useFlakeNixpkgs
-              ./home-manager/logan/makoto.nix
-            ];
-        };
-      "logan@orpheus" = home-manager.lib.homeManagerConfiguration {
-        pkgs = legacyPackages.x86_64-linux;
-        extraSpecialArgs = {inherit inputs;};
-        modules =
-          (builtins.attrValues homeManagerModules)
-          ++ [
-            useFlakeNixpkgs
-            ./home-manager/logan/orpheus.nix
           ];
       };
     };
