a21378cbff7676d8edc0329dbe72c7a6a9cb1adb
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2024-12-08 12:01:02 -0500

    new nerdfonts nixpkgs structure

--- package-groups/fonts.nix
+++ package-groups/fonts.nix
@@ -1,13 +1,19 @@
 {pkgs}:
-with pkgs; [
-  (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono" "Overpass" "NerdFontsSymbolsOnly"];})
-  apple-fonts
-  comic-mono
-  crimson-pro
-  fira-go
-  fira-sans
-  ibm-plex
-  iosevka-comfy.comfy
-  noto-fonts-emoji
-  sf-mono-liga
-]
+with pkgs;
+  [
+    apple-fonts
+    comic-mono
+    crimson-pro
+    fira-go
+    fira-sans
+    ibm-plex
+    iosevka-comfy.comfy
+    noto-fonts-emoji
+    sf-mono-liga
+  ]
+  ++ (with pkgs.nerd-fonts; [
+    fira-code
+    jetbrains-mono
+    overpass
+    symbols-only
+  ])
