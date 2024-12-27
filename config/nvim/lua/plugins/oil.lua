0ea7909346961f6281fcb46088f9be8ee9f70758
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2024-12-27 11:09:24 -0500

    delete to trash with oil

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,8 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {},
+	opts = {
+		delete_to_trash = true,
+	},
 	config = function(opts)
 		require("oil").setup(opts)
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
