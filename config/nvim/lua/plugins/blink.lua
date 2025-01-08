c21c247fb9bde7760cb8d49fad8d96f7340064f2
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2025-01-08 10:04:32 -0500

    update nvim plugins

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,7 +1,6 @@
 return {
 	"saghen/blink.cmp",
-	lazy = false, -- lazy loading handled internally
-	-- optional: provides snippets for the snippet source
+	lazy = false,
 	dependencies = "rafamadriz/friendly-snippets",
 	version = "v0.*",
 	-- If you use nix, you can build from source using latest nightly rust with:
@@ -10,58 +9,46 @@ return {
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
-		-- 'default' for mappings similar to built-in completion
-		-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-		-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-		-- see the "default configuration" section below for full documentation on how to define
-		-- your own keymap.
 		keymap = {
 			preset = "enter",
 			["<C-j>"] = { "select_next", "fallback" },
 			["<C-k>"] = { "select_prev", "fallback" },
 		},
-
-		highlight = { use_nvim_cmp_as_default = true },
-		-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-		-- adjusts spacing to ensure icons are aligned
-		nerd_font_variant = "mono",
-
-		-- default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, via `opts_extend`
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono",
+		},
 		sources = {
-			completion = {
-				enabled_providers = { "lsp", "path", "snippets", "buffer" },
-			},
+			default = { "lsp", "path", "snippets", "buffer" },
 			providers = {
-				-- Don't show LuaLS require statements when lazydev has items
-				lsp = { fallback_for = { "lazydev" } },
-				lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+				lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", fallbacks = { "lsp" } },
 			},
 		},
-		-- experimental auto-brackets support
-		-- accept = { auto_brackets = { enabled = true } }
 
-		-- experimental signature help support
-		trigger = { signature_help = { enabled = true } },
-		windows = {
-			autocomplete = {
+		completion = {
+			menu = {
 				border = "rounded",
 				winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
 			},
 			documentation = {
-				border = "rounded",
-				winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-			},
-			signature_help = {
-				border = "rounded",
-				winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+				window = {
+					border = "rounded",
+					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+				},
 			},
 			ghost_text = {
 				enabled = true,
 			},
 		},
+		signature = {
+			enabled = true,
+			window = {
+				border = "rounded",
+				winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+			},
+		},
 	},
 	-- allows extending the enabled_providers array elsewhere in your config
 	-- without having to redefine it
-	opts_extend = { "sources.completion.enabled_providers" },
+	opts_extend = { "sources.default" },
 }
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -123,7 +123,7 @@ return {
 		"nvim-telescope/telescope-ui-select.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
 		},
 	},
 }
