5ea6a2edd92a8bc3e7876c2d130b82f61dcb31fe
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2025-10-07 16:30:35 -0400

    update fidget

--- config/nvim/lua/plugins/fidget.lua
+++ config/nvim/lua/plugins/fidget.lua
@@ -1,7 +1,4 @@
 return {
-	{
-		"j-hui/fidget.nvim",
-		tag = "legacy",
-		opts = { text = { spinner = "dots" }, window = { blend = 0 } },
-	},
+	"j-hui/fidget.nvim",
+	opts = { text = { spinner = "dots" }, window = { blend = 0 } },
 }
