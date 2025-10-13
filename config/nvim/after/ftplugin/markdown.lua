d8784377c704b9ac63fb8ae51b40cc3bfab72efd
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2025-10-13 16:32:00 -0400

    disable spell check in markdown hoverdocs

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,11 @@
 local set = vim.opt_local
 
-set.spell = true -- Enable spellchecking
 set.textwidth = 80
 set.breakindent = true -- Wrapped lines preserve indentation
 -- set.breakindentopt = "sbr" -- Show symbol after broken lines
 -- set.showbreak = "⮑ "     -- Symbol to indicate line wrap
+
+local is_floating = vim.api.nvim_win_get_config(0).relative ~= ""
+if not is_floating then
+	set.spell = true -- Enable spellchecking
+end
