1a510058569394793a792d56585c68c9fe78e9e0
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2026-05-03 14:40:08 -0400

    update tyranitar config

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,22 +7,21 @@ if [ -n "$hostname" ]; then
     flake=".#$hostname"
 fi
 
+export NIX_CONFIG="extra-experimental-features = nix-command flakes"
+
 case "$(uname -s)" in
     Darwin)
-        subcommand=(darwin switch "$flake")
+        nix run nixpkgs#nh -- darwin switch "$flake"
         ;;
     Linux)
         if [ ! -e /etc/NIXOS ] && [ ! -e /etc/nixos ]; then
             echo "Unsupported Linux system: NixOS not detected" >&2
             exit 1
         fi
-        subcommand=(os switch --install-bootloader "$flake")
+        sudo nixos-rebuild switch --flake "$flake" --install-bootloader
         ;;
     *)
         echo "Unsupported system: $(uname -s)" >&2
         exit 1
         ;;
 esac
-
-export NIX_CONFIG="extra-experimental-features = nix-command flakes"
-nix run nixpkgs#nh -- "${subcommand[@]}"
--- host/tyranitar/configuration.nix
+++ host/tyranitar/configuration.nix
@@ -20,32 +20,31 @@
     };
   };
 
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot";
-    };
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+    powerManagement.enable = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices."luks-c4fd9340-9f7a-40bb-add8-6c8a04f48f8b".device = "/dev/disk/by-uuid/c4fd9340-9f7a-40bb-add8-6c8a04f48f8b";
 
   networking = {
     hostName = "tyranitar";
+    networkmanager.enable = true;
+    useDHCP = false;
     firewall = {
       trustedInterfaces = ["tailscale0"];
       allowedUDPPorts = [config.services.tailscale.port];
     };
   };
-
-  users.users = {
-    logan = {
-      isNormalUser = true;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEVj2KaxvkBVVDcMxcdhESfE80L38xXZN42jb8XfhUI logan"
-      ];
-      extraGroups = ["wheel"];
-    };
-  };
-
+  
   services = {
     openssh = {
       enable = true;
@@ -58,5 +57,70 @@
     tailscale.enable = true;
   };
 
-  system.stateVersion = "25.05";
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.logan = {
+    isNormalUser = true;
+    description = "logan";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "kvm" "video" "render"];
+    openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKEVj2KaxvkBVVDcMxcdhESfE80L38xXZN42jb8XfhUI logan"
+    ];
+    packages = with pkgs; [
+
+    ];
+  };
+
+  virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        runAsRoot = true;
+	    swtpm.enable = true;
+      };
+  };
+  virtualisation.spiceUSBRedirection.enable = true;
+  programs.virt-manager.enable = true;
+  
+  programs.firefox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+  ];
+
+  system.stateVersion = "25.11";
 }
